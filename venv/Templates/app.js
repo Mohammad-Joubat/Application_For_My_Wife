44b24eab358335d5f8a52d0b0e15ebb6f299afbc
Mohammad-Joubat <68898478+Mohammad-Joubat@users.noreply.github.com>
2023-02-02 23:23:18 +0200

    Delete app.js

--- venv/Templates/app.js
+++
@@ -1,9 +0,0 @@
-<script>
-  const times = document.querySelectorAll(".time");
-  times.forEach(time => {
-    time.addEventListener("click", function() {
-      times.forEach(t => t.classList.remove("selected"));
-      this.classList.add("selected");
-    });
-  });
-</script>
